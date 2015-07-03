4ac75061f14c3bee0a676f190b526ba4d4d604f4
Duncan Neal <duncan.neal@gmail.com>
2015-07-02 21:37:26 -0400

    Added completed functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-
+devise_for :users
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
+      member do
+        patch :complete
+      end
+    end
   end  
-  devise_for :users
-
-
+ 
 
   root "todo_lists#index"
 end
